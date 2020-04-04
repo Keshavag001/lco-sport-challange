474095659bec63357a30b5362088989449375d36
Yash Agrawal <agrawalkeshav001@gmail.com>
2020-04-04 22:01:00 +0530

    Updates

---
+++ precache-manifest.456172c269cf87ab033f480c01e08fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae9f2e106205ffc49ec6a8218d6242d",
+    "url": "/lco-sport-challange/index.html"
+  },
+  {
+    "revision": "dcd4bf1e30a54fc6ba2a",
+    "url": "/lco-sport-challange/static/css/main.d359647b.chunk.css"
+  },
+  {
+    "revision": "233e686c7e0e32ffed9e",
+    "url": "/lco-sport-challange/static/js/2.a8dfb339.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lco-sport-challange/static/js/2.a8dfb339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd4bf1e30a54fc6ba2a",
+    "url": "/lco-sport-challange/static/js/main.f2c2cb06.chunk.js"
+  },
+  {
+    "revision": "fc93e08f41945b72def1",
+    "url": "/lco-sport-challange/static/js/runtime-main.752f8f77.js"
+  }
+]);
